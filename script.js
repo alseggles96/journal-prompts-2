eea79ca68f1dd9895e95340c34c79b59e2a08659
Amber Austin <amberaustin@Ambers-MacBook-Pro.local>
2025-02-26 19:33:10 -0600

    Fixed missing comma in prompts array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generatePrompt() {
     const prompts = [
         "Whats your favorite thing to do on a rainy day?",
-        "What is something that drives you?"
+        "What is something that drives you?",
         "If you could be any animal, what would you be and why?",
         "Write about a time you made someone laugh.",
         "Whats a superpower you wish you had?",
