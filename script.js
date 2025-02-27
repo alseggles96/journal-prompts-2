ef2c0718da1edf363773de8d9d56eb1c02d69678
Amber Austin <amberaustin@Ambers-MacBook-Pro.local>
2025-02-26 21:40:28 -0600

    automated prompts 2?

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function generatePrompt() {
-    fetch('https://api.quotable.io/random?tags=inspirational') // Free API for quotes
-        .then(response => response.json()) // Turn the response into usable data
+    fetch('https://api.quotable.io/random?tags=inspirational') // Fixed URL with https://
+        .then(response => response.json())
         .then(data => {
-            const prompt = data.content; // The quote itself
-            document.getElementById("prompt").innerHTML = prompt; // Show it
+            const prompt = data.content;
+            document.getElementById("prompt").innerHTML = prompt;
         })
         .catch(error => {
-            console.log("Oops, something went wrong!", error); // If it fails
+            console.log("Oops, something went wrong!", error);
             document.getElementById("prompt").innerHTML = "Oopsie, try again, girly!";
         });
 }
